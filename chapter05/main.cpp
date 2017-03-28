4338620350c4e4ad28ede904c25776855b577fa8
yanjunyang.yyj <yanjunyang.yyj@alibaba-inc.com>
2017-03-28 11:05:28 +0800

    chapter05 -

---
+++ chapter05/main.cpp
@@ -0,0 +1,159 @@
+//
+// Created by yangyanjun on 2017/3/28.
+//
+
+
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// STL
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+
+//
+#include "scene.h"
+#include "glutils.h"
+
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+Scene *scene;
+GLFWwindow *window;
+
+std::string parseCLArgs(int argc, char **argv);
+void printHelpInfo(const char *);
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_SPACE && action == GLFW_RELEASE)
+    {
+        if (scene)
+            scene->animate( !(scene->animating()) );
+    }
+}
+
+void initializeGL() {
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+#ifndef __APPLE__
+    glDebugMessageCallback(GLUtils::debugCallback, NULL);
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+    glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_MARKER, 0,
+                         GL_DEBUG_SEVERITY_NOTIFICATION, -1 , "Start debugging");
+#endif
+    scene->initScene();
+}
+
+void mainLoop() {
+    while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_SPACE)) {
+        GLUtils::checkForOpenGLError(__FILE__, __LINE__);
+        scene->update(float(glfwGetTime()));
+        scene->render();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
+
+void resizeGL(int w, int h) {
+    scene->resize(w, h);
+}
+
+int main(int argc, char * argv[]) {
+    std::string recipe = parseCLArgs(argc, argv);
+
+    //
+    glfwInit();
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+#else
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#endif
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Open the window
+    std::string title = "Chapter 05 -- " + recipe;
+    window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, title.c_str(), nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    // Load OpenGL Functions
+    gladLoadGL();
+    std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+
+    // Viewport
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    GLUtils::dumpGLInfo();
+
+    // Initialization
+    initializeGL();
+    resizeGL(width, height);
+
+    // Enter the main loop
+    mainLoop();
+
+    // Close window and terminate GLFW
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
+
+std::string parseCLArgs(int argc, char ** argv) {
+
+    if( argc < 2 ) {
+        printHelpInfo(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    std::string recipe = argv[1];
+
+    if( recipe == "blur") {
+//        scene = new SceneBlur();
+    } else if( recipe == "deferred") {
+//        scene = new SceneDeferred();
+    } else if( recipe == "edge" ) {
+//        scene = new SceneEdge();
+    } else if( recipe == "gamma") {
+//        scene = new SceneGamma();
+    } else if( recipe == "msaa" ) {
+//        scene = new SceneMsaa();
+    } else if( recipe == "tone-map" ) {
+//        scene = new SceneToneMap();
+    } else if( recipe == "hdr-bloom" ) {
+//        scene = new SceneHdrBloom();
+    } else if( recipe == "oit" ) {
+//        scene = new SceneOit();
+    } else {
+        printf("Unknown recipe: %s\n", recipe.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    return recipe;
+}
+
+void printHelpInfo(const char * exeFile) {
+    printf("Usage: %s recipe-name\n\n", exeFile);
+    printf("Recipe names: \n");
+    printf("  blur     : Gaussian blur\n");
+    printf("  deferred : deferred rendering\n");
+    printf("  edge     : edge detection filter\n");
+    printf("  gamma    : gamma correction\n");
+    printf("  msaa     : multisample anti-aliasing\n");
+    printf("  tone-map : tone mapping example.\n");
+    printf("  hdr-bloom: bloom example with HDR tone mapping.\n");
+    printf("  oit      : order independent transparency\n");
+}
+
---
+++ common/Meshes/vbosphere.h
@@ -0,0 +1,25 @@
+#ifndef VBOSPHERE_H
+#define VBOSPHERE_H
+
+#include "drawable.h"
+#include <glad/glad.h>
+
+class VBOSphere : public Drawable
+{
+private:
+    unsigned int vaoHandle;
+    GLuint nVerts, elements;
+	float radius;
+	GLuint slices, stacks;
+
+    void generateVerts(float * , float * ,float *, GLuint *);
+
+public:
+    VBOSphere(float, GLuint, GLuint);
+
+    void render() const;
+
+    int getVertexArrayHandle();
+};
+
+#endif // VBOSPHERE_H
